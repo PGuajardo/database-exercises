9437b3cb8be6c31dbd0c539bdf659598e962f168
Paige Guajardo <paigeeguajardo@gmail.com>
2021-09-17 14:00:28 -0500

    Updated tables_exercises.sql

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,5 +1,13 @@
+-- Use the employees DB
+USE employees;
+
+-- List the tables in the DB
 show tables;
+
+-- Exploring the Employees table
 describe employees;
+
+-- Exploring the other tables and their datatypes
 describe departments;
 describe salaries;
 describe titles;
@@ -7,3 +15,7 @@ describe dept_manager;
 describe dept_emp_latest_date;
 describe dept_emp;
 describe current_dept_emp;
+
+-- Relation ship between employee and dept is in dept_emp which uses both id numbers
+-- Showing the sql that created the dept_manager
+show create table dept_manager;
