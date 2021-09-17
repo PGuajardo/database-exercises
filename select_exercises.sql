5c13385c1e8d873baa91a779bf59cc294919180b
Paige Guajardo <paigeeguajardo@gmail.com>
2021-09-17 14:45:46 -0500

    Select Exercises queries

---
+++ select_exercises.sql
@@ -0,0 +1,34 @@
+USE albums_db;
+
+SHOW tables;
+DESCRIBE albums;
+-- a. How many rows are in the albums table?
+# 31 Rows
+SELECT * FROM albums;
+-- b. How many unique artist names are in the albums table?
+# 31
+SELECT DISTINCT arist FROM albums;
+-- c. What is the primary key for the albums table?
+SELECT id FROM albums;
+-- d. What is the oldest release date for any album in the albums table? What is the most recent release date?
+
+SELECT * FROM albums WHERE release_date = '2011';
+SELECT * FROM albums WHERE release_date = '1967';
+-- 4
+-- a. The name of all albums by Pink Floyd
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+-- b. The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+
+-- c. The genre for the album Nevermind
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+-- d. Which albums were released in the 1990s
+SELECT * FROM albums WHERE release_date > 1990;
+
+-- e. Which albums had less than 20 million certified sales
+SELECT * FROM albums WHERE sales > 20;
+
+-- f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT * FROM albums WHERE genre = 'Rock';
