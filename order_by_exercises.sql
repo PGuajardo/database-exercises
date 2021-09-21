c8f4881f3276face0484c8834a639ce3d24229a2
Paige Guajardo <paigeeguajardo@gmail.com>
2021-09-21 16:59:27 -0500

    Modified order_by with group_by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -77,3 +77,66 @@ first_name, last_name, birth_date
 FROM employees
 LIMIT 10;  
 
+--
+--
+
+-- 1. In your script, use DISTINCT to find the unique titles in the titles table. How many unique titles have there ever been? Answer that in a comment in your SQL file.
+use titles;
+SELECT DISTINCT title
+FROM titles;
+-- 7 
+
+-- 2. Write a query to to find a list of all unique last names of all employees that start and end with 'E' using GROUP BY.
+
+SELECT /*first_name,*/ last_name FROM employees
+GROUP BY last_name #first_name
+HAVING last_name LIKE 'E%%E';
+-- 5
+
+-- 3. Write a query to to find all unique combinations of first and last names of all employees whose last names start and end with 'E'.
+
+SELECT first_name, last_name FROM employees
+GROUP BY last_name, first_name
+HAVING last_name LIKE 'E%%E';
+
+-- 4. Write a query to find the unique last names with a 'q' but not 'qu'. Include those names in a comment in your sql code.
+
+SELECT last_name FROM employees
+GROUP BY last_name
+HAVING last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+-- Chleq, Lindqvist, Qiwen
+
+-- 5. Add a COUNT() to your results (the query above) to find the number of employees with the same last name.
+
+SELECT last_name, COUNT(last_name) AS COUNT_OF_LAST 
+FROM employees
+GROUP BY last_name;
+-- 1637
+
+-- 6. Find all all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
+
+SELECT first_name, COUNT(*), gender FROM employees
+GROUP BY first_name, gender
+# ORDER BY COUNT(*) ASC
+HAVING first_name IN ('Irena', 'Vidya', 'Maya');
+
+-- 7. Using your query that generates a username for all of the employees, generate a count employees for each unique username. Are there any duplicate usernames? BONUS: How many duplicate usernames are there?
+
+SELECT LOWER(CONCAT(SUBSTR(first_name,1,1), SUBSTR(last_name,1,4), '_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2))) AS username, COUNT(*) 
+FROM employees
+GROUP BY username;
+# 285872
+
+# HAVING COUNT(*) > 1;
+# 13251
+
+
+-- More practice with aggregate functions:
+
+/*Find the historic average salary for all employees. Now determine the current average salary.
+Now find the historic average salary for each employee. Reminder that when you hear "for each" in the problem statement, you'll probably be grouping by that exact column.
+Find the current average salary for each employee.
+Find the maximum salary for each current employee.
+Now find the max salary for each current employee where that max salary is greater than $150,000.
+Find the current average salary for each employee where that average salary is between $80k and $90k.*/
+
