93b9cb68e5afe4210d3de08f0b8d248a8b98bb72
Paige Guajardo <paigeeguajardo@gmail.com>
2021-09-21 15:16:18 -0500

    Updated functions.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -50,25 +50,26 @@ SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' OR
 use employees;
 
 -- 1. Write a query to to find all employees whose last name starts and ends with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
-SELECT concat(first_name, last_name) AS full_name 
+SELECT concat(first_name,' ', last_name) AS full_name 
 FROM employees 
-WHERE last_name LIKE 'E%%E';
+WHERE last_name LIKE 'E%E';
 -- 899
 
 -- 2. Convert the names produced in your last query to all uppercase.
-SELECT UPPER(concat(first_name, last_name)) AS full_name 
+SELECT UPPER(concat(first_name,' ', last_name)) AS full_name 
 FROM employees 
 WHERE last_name LIKE 'E%%E';
 
 -- 3. Find all employees hired in the 90s and born on Christmas. Use datediff() function to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE()),
 use employees;
-SELECT DATEDIFF(CURDATE(), hire_date) FROM employees
-WHERE (hire_date LIKE '199%') AND (birth_date LIKE '%-25');
+SELECT first_name, last_name, DATEDIFF(CURDATE(), hire_date) AS Days_Working FROM employees
+WHERE (hire_date LIKE '199%') AND (birth_date LIKE '%-12-25');
 
 -- 4. Find the smallest and largest current salary from the salaries table.
-
+use employees;
 SELECT MIN(salary) AS SmallestSalary, MAX(salary) AS LargestSalary 
-FROM salaries;
+FROM salaries
+WHERE to_date() > CURDATE();
 
 -- 5. Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like: hhhhhh
 
