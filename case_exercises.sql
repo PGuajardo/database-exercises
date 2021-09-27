a8477f3fed8f89f6f53a3e35b64d4166c7dfd126
Paige Guajardo <paigeeguajardo@gmail.com>
2021-09-27 17:10:10 -0500

    Uploaded case_exercises.sql file

---
+++ case_exercises.sql
@@ -0,0 +1,34 @@
+
+-- 1. Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.
+USE employees;
+
+SELECT emp_no, dept_no, hire_date, to_date,
+		IF(dept_emp.to_date > NOW(), True, False) AS is_current_employee
+FROM employees
+JOIN dept_emp using(emp_no);
+
+-- 2. Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
+
+SELECT first_name, last_name,
+	CASE
+		WHEN last_name LIKE 'A%' OR last_name LIKE 'B%' OR last_name LIKE 'C%' OR last_name LIKE 'D%' OR last_name LIKE 'E%' OR last_name LIKE 'F%' OR last_name LIKE 'G%' 
+			OR last_name LIKE 'H%' THEN 'A-H'
+		WHEN last_name LIKE 'I%' OR last_name LIKE 'J%' OR last_name LIKE 'K%' OR last_name LIKE 'L%' OR last_name LIKE 'M%' OR last_name LIKE 'N%' OR last_name LIKE 'O%'
+			OR last_name LIKE 'P%' OR last_name LIKE 'Q%' THEN 'I-Q'
+		WHEN last_name LIKE 'R%' OR last_name LIKE 'S%' OR last_name LIKE 'T%' OR last_name LIKE 'U%' OR last_name LIKE 'V%' OR last_name LIKE 'W%' OR last_name LIKE 'X%'
+			OR last_name LIKE 'Y%' OR last_name LIKE 'Z%' THEN 'R-Z'
+		ELSE 'OTHER'
+		END AS alpha_group
+FROM employees;
+
+-- 3. How many employees (current or previous) were born in each decade?
+
+SELECT birth_date,
+	COUNT(CASE birth_date LIKE '195%' THEN 'Born in 1950s') AS '1950s',
+	COUNT(CASE birth_date LIKE '196%' THEN 'Born in 1960s') AS '1960s'
+FROM employees
+GROUP BY birth_date
+ORDER BY birth_date ASC;
+###BONUS
+
+-- What is the current average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
