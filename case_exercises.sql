1e0cd1041de2a3987373ccc2da5721ab068a8207
Paige Guajardo <paigeeguajardo@gmail.com>
2021-09-29 12:30:58 -0500

    Updated case_exercises.sql

--- case_exercises.sql
+++ case_exercises.sql
@@ -2,10 +2,22 @@
 -- 1. Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.
 USE employees;
 
-SELECT emp_no, dept_no, hire_date, to_date,
+# SELECT dept_no FROM dept_emp WHERE MAX(to_date) GROUP BY dept_no;
+
+SELECT emp_no, dept_no , hire_date, to_date,
 		IF(dept_emp.to_date > NOW(), True, False) AS is_current_employee
-FROM employees
-JOIN dept_emp using(emp_no);
+FROM dept_emp;
+
+
+
+
+SELECT dept_emp.emp_no, dept_no, to_date, hire_date,
+		IF(dept_emp.to_date > NOW(), 1, 0) AS is_current_employee
+FROM dept_emp
+JOIN (SELECT emp_no, MAX(to_date) AS max_date 
+FROM dept_emp
+GROUP BY emp_no) AS last_dept ON last_dept.emp_no = dept_emp.emp_no AND dept_emp.to_date = last_dept.max_date
+JOIN employees ON employees.emp_no = dept_emp.emp_no;
 
 -- 2. Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
 
